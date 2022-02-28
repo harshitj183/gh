d76f53c979231d95fe61e902e4326b6e7d53dc77
Harshit Jaiswal (@harshitj183) <76927137+harshitj183@users.noreply.github.com>
2022-02-28 07:00:49 +0530

    Create Allpagejavascript.js

---
+++ Allpagejavascript.js
@@ -0,0 +1,50 @@
+#bookmark js#
+var bookmarkLazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===bookmarkLazy||0!=document.body.scrollTop&&!1===bookmarkLazy)&&(!function(){var d=document.createElement("script");d.type="text/javascript",d.async=!0,d.src="https://cdn.statically.io/gh/harshitj183/gh/main/bookmark.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(d,a)}(),bookmarkLazy=!0)},!0);
+
+
+#push notification#
+
+var pushLazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===pushLazy||0!=document.body.scrollTop&&!1===pushLazy)&&(!function(){var c=document.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://static.cleverpush.com/channel/loader/HbcuCnZ4bmt35RuyB.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(c,b)}(),pushLazy=!0)},!0);
+
+
+#ads adskeeper#
+
+
+
+var adskeeperLazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===adskeeperLazy||0!=document.body.scrollTop&&!1===adskeeperLazy)&&(!function(){var c=document.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://cdn.statically.io/js/jsc.adskeeper.co.uk/h/a/harshitj183.eu.org.1269498.es5.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(c,b)}(),adskeeperLazy=!0)},!0);
+
+
+
+
+
+#ads infolink#
+
+
+
+var infolinkLazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===infolinkLazy||0!=document.body.scrollTop&&!1===infolinkLazy)&&(!function(){var c=document.createElement("script");c.type="text/javascript",c.async=!0,c.src="//resources.infolinks.com/js/infolinks_main.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(c,b)}(),infolinkLazy=!0)},!0);
+
+
+#ads formedia#
+var formediaLazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===formediaLazy||0!=document.body.scrollTop&&!1===formediaLazy)&&(!function(){var c=document.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://cdn.statically.io/js/platform.foremedia.net/code/11128/analytics";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(c,b)}(),formediaLazy=!0)},!0);
+
+
+
+
+ 
+#speedup#
+ var autosavelazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===autosavelazy||0!=document.body.scrollTop&&!1===autosavelazy)&&(!function(){var c=document.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://cdn.statically.io/js/cdn.jsdelivr.net/npm/js-autosave/src/js.autosave.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(c,b)}(),autosavelazy=!0)},!0);
+  
+  
+  
+  
+  var Momentspeedlazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===Momentspeedlazy||0!=document.body.scrollTop&&!1===Momentspeedlazy)&&(!function(){var c=document.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://cdn.statically.io/js/MomentJS.com/downloads/moment.min.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(c,b)}(),Momentspeedlazy=!0)},!0);/*]]>*/</script>
+  
+  
+
+
+
+var Instaintlazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===Instaintlazy||0!=document.body.scrollTop&&!1===Instaintlazy)&&(!function(){var c=document.createElement("script");c.integrity='sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw';c.type="module",c.async=!0,c.src="https://cdn.statically.io/js/instant.page/5.1.0";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(c,b)}(),Instaintlazy=!0)},!0);
+
+
+var Quiklinklazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===Quiklinklazy||0!=document.body.scrollTop&&!1===Quiklinklazy)&&(!function(){var c=document.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://cdn.statically.io/js/cdnjs.cloudflare.com/ajax/libs/quicklink/2.2.0/quicklink.umd.min.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(c,b)}(),Quiklinklazy=!0)},!0);/*]]>*/</script>
+  window.addEventListener(&#39;load&#39;,()=&gt;{quicklink.listen();});
