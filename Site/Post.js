5f7b32d8f9ae3ae39a035784302466b78f9985b9
Harshit Jaiswal (@harshitj183) <76927137+harshitj183@users.noreply.github.com>
2022-03-01 20:50:00 +0530

    Update Post.js

--- Site/Post.js
+++ Site/Post.js
@@ -6,7 +6,7 @@
 
  
 
-function get_text(el) {ret = ""; var length = el.childNodes.length; for(var i = 0; i < length; i++) {var node = el.childNodes[i]; if(node.nodeType != 8) {ret += node.nodeType != 1 ? node.nodeValue : get_text(node);} } return ret;} var words = get_text(document.getElementById('post-body')); var count = words.split(' ').length; var avg = 200; var counted = count / avg; var maincount = Math.round(counted); document.getElementById("read-time").innerHTML = "&nbsp;" + maincount + " minutes to read"; 
+!function get_text(el) {ret = ""; var length = el.childNodes.length; for(var i = 0; i < length; i++) {var node = el.childNodes[i]; if(node.nodeType != 8) {ret += node.nodeType != 1 ? node.nodeValue : get_text(node);} } return ret;} var words = get_text(document.getElementById('post-body')); var count = words.split(' ').length; var avg = 200; var counted = count / avg; var maincount = Math.round(counted); document.getElementById("read-time").innerHTML = "&nbsp;" + maincount + " minutes to read"; 
 
 /*! menate to read  */
 
