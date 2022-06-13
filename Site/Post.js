597e17a9da5909318f9a1642bcc1ab41a88c99d5
Harshit Jaiswal (@harshitj183) <76927137+harshitj183@users.noreply.github.com>
2022-06-13 19:02:47 +0530

    Update Post.js

--- Site/Post.js
+++ Site/Post.js
@@ -1,3 +1,2 @@
-//<![CDATA[ 
+
 (function gourabdesignmultiRelated() {var text = '🩸𝔸𝕝𝕤𝕠 ℝ𝕖𝕒𝕕 &amp;#9889;';let r = Math['floor']((relatedTitles['length'] - 1) * Math['random']());let i = 0;let jumlah = document['querySelectorAll']('.gourabdesignmultiRelated');while (i < relatedTitles['length'] && i < jumlah['length']) {for (let a = 0; a < jumlah['length']; a++) {jumlah[a]['innerHTML'] = '<span class="content"><span class="text">' + text + '</span><a href="' + relatedUrls[r] + '" title="' + relatedTitles[r] + '">' + relatedTitles[r] + '</a></span><span class="icon"></span>';if (r < relatedTitles['length'] - 1) {r++} else {r = 0};i++}}})(); 
-//]]> 
