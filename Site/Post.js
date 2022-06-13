20c6cc5058155706b4a5a720afde33177715514b
Harshit Jaiswal (@harshitj183) <76927137+harshitj183@users.noreply.github.com>
2022-06-13 10:59:52 +0530

    Update Post.js

--- Site/Post.js
+++ Site/Post.js
@@ -1,12 +1,1 @@
-/*! Table of Contents | v0.4.1 - https://github.com/ndabas/toc */
-!function(t){"use strict";var n=function(n){return this.each(function(){var e,i,a=t(this),o=a.data(),c=[a],r=this.tagName,d=0;e=t.extend({content:"body",headings:"h1,h2,h3"},{content:o.toc||void 0,headings:o.tocHeadings||void 0},n),i=e.headings.split(","),t(e.content).find(e.headings).attr("id",function(n,e){return e||function(t){0===t.length&&(t="?");for(var n=t.replace(/[^a-zA-Z ]/g,"").replace(/\s+/g,"_"),e="",i=1;null!==document.getElementById(n+e);)e="_"+i++;return n+e}(t(this).text())}).each(function(){var n=t(this),e=t.map(i,function(t,e){return n.is(t)?e:void 0})[0];if(e>d){var a=c[0].children("li:last")[0];a&&c.unshift(t("<"+r+"/>").appendTo(a))}else c.splice(0,Math.min(d-e,Math.max(c.length-1,0)));t("<li/>").appendTo(c[0]).append(t("<a/>").text(n.text()).attr("href","#"+n.attr("id"))),d=e})})},e=t.fn.toc;t.fn.toc=n,t.fn.toc.noConflict=function(){return t.fn.toc=e,this},t(function(){n.call(t("[data-toc]"))})}(window.jQuery);
-
-
-/*! menate to read  */
-
- 
-
-!function get_text(el) {ret = ""; var length = el.childNodes.length; for(var i = 0; i < length; i++) {var node = el.childNodes[i]; if(node.nodeType != 8) {ret += node.nodeType != 1 ? node.nodeValue : get_text(node);} } return ret;} var words = get_text(document.getElementById('post-body')); var count = words.split(' ').length; var avg = 200; var counted = count / avg; var maincount = Math.round(counted); document.getElementById("read-time").innerHTML = "&nbsp;" + maincount + " minutes to read"; 
-
-/*! menate to read  */
-
+(function gourabdesignmultiRelated(){var text=&#39;🩸𝔸𝕝𝕤𝕠 ℝ𝕖𝕒𝕕 &amp;#9889;&#39;;let r=Math[&#39;floor&#39;]((relatedTitles[&#39;length&#39;]-1)*Math[&#39;random&#39;]());let i=0;let jumlah=document[&#39;querySelectorAll&#39;](&#39;.gourabdesignmultiRelated&#39;);while(i&lt;relatedTitles[&#39;length&#39;]&amp;&amp;i&lt;jumlah[&#39;length&#39;]){for(let a=0;a&lt;jumlah[&#39;length&#39;];a++){jumlah[a][&#39;innerHTML&#39;]=&#39;&lt;span class=&quot;content&quot;&gt;&lt;span class=&quot;text&quot;&gt;&#39;+text+&#39;&lt;/span&gt;&lt;a href=&quot;&#39;+relatedUrls[r]+&#39;&quot; title=&quot;&#39;+relatedTitles[r]+&#39;&quot;&gt;&#39;+relatedTitles[r]+&#39;&lt;/a&gt;&lt;/span&gt;&lt;span class=&quot;icon&quot;&gt;&lt;/span&gt;&#39;;if(r&lt;relatedTitles[&#39;length&#39;]-1){r++}else{r=0};i++}}})();
