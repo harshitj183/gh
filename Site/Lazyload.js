6f4ecfc69b9b9f4619fb05891873b2da9a5ed85f
Harshit Jaiswal (@harshitj183) <76927137+harshitj183@users.noreply.github.com>
2022-06-28 15:44:35 +0530

    Update Lazyload.js

--- Site/Lazyload.js
+++ Site/Lazyload.js
@@ -22,4 +22,3 @@ window.onscroll=ATscrollProgress;window.onload=ATscrollProgress;/*]]>*/
     }
     document.head.append(s)
 
-/*<!CDATA[*/(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=gtm2";y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","9umzzljbtg");/*]]>*/
